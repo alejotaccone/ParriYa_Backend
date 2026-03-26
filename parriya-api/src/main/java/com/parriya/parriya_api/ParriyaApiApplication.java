be29af2ce9214cc6d87e9000f4e97ce025adb274
alejotaccone <alejotaccone11@gmail.com>
2026-03-26 10:18:44 -0300

    Inicializacion de SpringBoot

---
+++ parriya-api/src/main/java/com/parriya/parriya_api/ParriyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.parriya.parriya_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParriyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParriyaApiApplication.class, args);
+	}
+
+}
---
+++ parriya-api/src/test/java/com/parriya/parriya_api/ParriyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parriya.parriya_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParriyaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
